b16c975d31e87de703642c4cff4c17532ff143b1
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-03 00:51:58 -0700

    /login and /logout paths boilerplates

--- application/__init__.py
+++ application/__init__.py
@@ -4,3 +4,5 @@ from flask.ext.mongoengine import MongoEngine
 app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
 db = MongoEngine(app)
+
+from application import login
--- application/login.py
+++ application/login.py
@@ -2,4 +2,8 @@ from application import app
 
 @app.route("/login", methods=["POST","GET"])
 def login():
-    
+    return "i am a login page"
+
+@app.route("/logout")
+def logout():
+    return "log out"
