740524c8e489395c4edbe5ee1b7144077908f2ff
Michael <mhardy17@bayschoolsf.org>
2015-04-03 20:36:30 -0700

    added example templates

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
 db = MongoEngine(app)
 
-from application import login
+from application import login, home
---
+++ application/home.py
@@ -0,0 +1,11 @@
+from application import app
+from flask import render_template
+
+@app.route('/')
+def index():
+	return render_template('index.html')#variables go after template
+
+@app.route('/u/')
+@app.route('/u/<name>')
+def profile(name=None):
+	return render_template('user.html', name=name)
