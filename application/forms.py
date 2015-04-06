a9149951e8ec0f73b59f797ce814a072e4a38260
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-06 09:04:33 -0700

    email -> String

--- application/forms.py
+++ application/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, EmailField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    email = EmailField("email", validators=[DataRequired()])
+    email = StringField("email", validators=[DataRequired()])
     password = PasswordField("password", validators=[DataRequired()])
 
 class SignupForm(Form):
