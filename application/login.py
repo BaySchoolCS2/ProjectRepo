4ba5ca704d82bc9bc88930749ad3118ecc8b513f
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-06 00:48:08 -0700

    login stuff

--- application/login.py
+++ application/login.py
@@ -1,8 +1,15 @@
 from application import app
-
+from flask import session, render_template
+from forms import LoginForm
 @app.route("/login", methods=["POST","GET"])
 def login():
-    
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+        #what happens after form is submitted goes here
+        
+    return render_template("login.html", form=form)
+
 @app.route("/logout")
 def logout():
     return "log out"
