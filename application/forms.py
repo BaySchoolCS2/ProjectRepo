94aa03be0bc8ff1b9c0973d99d053a1115052189
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-06 09:13:07 -0700

    I should really test this stuff

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, EmailField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
