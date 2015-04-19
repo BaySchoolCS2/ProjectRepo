556147d9328bfc01289a4b4190267ed2faa31116
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-19 10:47:39 -0700

    multiple affirmatives for deleteDB

--- tools/deleteDB.py
+++ tools/deleteDB.py
@@ -4,5 +4,5 @@ import pymongo
 print('This tool DELETES THE WHOLE DATABASE!  Use this tool ONLY IN LOCAL TESTING!')
 Uin = raw_input('Are you sure you want to delete the WHOLE database [Y/n]')
 Uin = Uin.lower()
-if Uin == 'y':
+if Uin in ["y", "yes", "yolo", "yolo :)"]:
 	pymongo.MongoClient().drop_database('project')
