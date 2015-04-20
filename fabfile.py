32517549e9fa969cd4b6a736e8226581a85ed336
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-19 19:08:13 -0700

    fabric for things? i think yes

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,11 @@
+#! /usr/bin/python
 
+from fabric.api import local
+
+def update_reqs():
+    '''Updates requirements.txts'''
+    local('./utils/upgrade.sh')
+
+def setup_env():
+    '''sets up environment'''
+    local('./utils/upgrade.sh')
