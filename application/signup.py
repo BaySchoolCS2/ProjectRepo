80be4ad569138de07420412a284e42e24339daa4
Michael <mhardy17@bayschoolsf.org>
2015-04-21 10:45:41 -0700

    temporary fix for bad email address error

--- application/signup.py
+++ application/signup.py
@@ -1,6 +1,6 @@
 from application import app
 from collections import User
-from flask import session, render_template, redirect
+from flask import session, render_template, redirect, flash
 from forms import SignupForm
 from werkzeug.security import generate_password_hash
 
@@ -13,6 +13,11 @@ def signup():
         if form.password.data == form.password2.data:
             pw_hash = generate_password_hash(form.password.data)
             user = User(email = form.email.data, alias = form.alias.data, password = pw_hash)
-            user.save()
-            return redirect(url_for("login"))
+            try:
+                user.save()
+                return redirect(url_for("login"))
+            except Exception, e:
+                print('exception %s' % str(e))
+                if 'Invalid Mail-address' in str(e):
+                    flash('Invalid email')
     return render_template('signup.html', form = form)
