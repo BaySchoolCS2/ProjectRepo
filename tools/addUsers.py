36b76ca51724328c70fb4860e019f4da9947d87e
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-21 13:24:42 -0700

    addUsers now uses mongoengine

--- tools/addUsers.py
+++ tools/addUsers.py
@@ -1,19 +1,25 @@
 #! tools-env/bin/python
 
-import pymongo
+from mongoengine import connect, Document, EmailField, StringField, BooleanField, DateTimeField
 from werkzeug.security import generate_password_hash
 
 usernames_file = open("tools/libs/usernames", "r")
 
 users = []
 
-for line in usernames_file:
-    if not(pymongo.MongoClient().project.User.find_one({'alias':line.split(":")[0]})):
-        if line.split(':')[3].strip() == 'true':
-            verified = True
-        else:
-            verified = False
-        users.append({"alias":line.split(":")[0],"email":line.split(":")[1],"password":generate_password_hash(line.split(":")[2]), "emailVerified":verified})
+class User(Document):
+    email = EmailField(required = True, unique = True)
+    alias = StringField(required = True, unique = True)
+    password = StringField(min_length = 8, required = True)
+    emailVerified = BooleanField(default = False)
+    lastLogin = DateTimeField()
+    allowTracking = BooleanField(default = False)
+    isMod = BooleanField(default = False)
 
-for user in users:
-    pymongo.MongoClient().project.User.insert(user)
+for line in usernames_file:
+    for line in usernames_file:
+        pw_hash = generate_password_hash(line.split(":")[2])
+        try:
+            User(email=line.split(":")[1], alias=line.split(":")[0], password=pw_hash, isMod=line.split(":")[3]).save()
+        except:
+            print "failed to create user %s" % line.split(":")[0]
