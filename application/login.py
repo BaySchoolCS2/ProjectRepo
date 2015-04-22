06e250e1f12744ccfa7a5c2d9bdea38a06b30723
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-22 13:55:14 -0700

    docstrings :)

--- application/login.py
+++ application/login.py
@@ -5,7 +5,23 @@ from forms import LoginForm
 from werkzeug.security import check_password_hash
 
 @app.route('/login', methods=['POST', 'GET'])
+'''
+    when /login is accessed run login
+'''
 def login():
+    '''
+        form is imported from froms.py
+        session["logged_in"] is a boolean wether or not someone is logged in
+        if you are already logged in you dont need to login, so you are redirected
+        to index
+
+        session['alias'] is the alias of the user that is currently logged in
+        session['allowTracking'] is wether or not the user is to be tracked
+        session['isMod'] is wether or not user is a moderator/advanced permission
+        flash returns a string to be delayed
+
+        
+    '''
     form = LoginForm()
     if session.get('logged_in'):
         return redirect(url_for('index'))
