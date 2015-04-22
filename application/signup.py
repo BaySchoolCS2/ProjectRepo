0832b0fee2e431e99dcbc40a1ce99bec2671fe97
Michael <mhardy17@bayschoolsf.org>
2015-04-22 14:12:44 -0700

    changes ?

--- application/signup.py
+++ application/signup.py
@@ -1,11 +1,7 @@
 from application import app
 from collections import User
-<<<<<<< HEAD
-from flask import session, render_template, redirect, flash
-=======
 from mongoengine import ValidationError, errors
 from flask import session, render_template, redirect, flash, url_for
->>>>>>> 052341e8a75119f7129260ff60e92397019ab30b
 from forms import SignupForm
 from werkzeug.security import generate_password_hash
 
@@ -17,16 +13,6 @@ def signup():
     if form.validate_on_submit():
         if form.password.data == form.password2.data and len(form.password.data) >= 8:
             pw_hash = generate_password_hash(form.password.data)
-<<<<<<< HEAD
-            user = User(email = form.email.data, alias = form.alias.data, password = pw_hash)
-            try:
-                user.save()
-                return redirect(url_for("login"))
-            except Exception, e:
-                print('exception %s' % str(e))
-                if 'Invalid Mail-address' in str(e):
-                    flash('Invalid email')
-=======
             try:
                 user = User(email = form.email.data, alias = form.alias.data, password = pw_hash)
                 user.save()
@@ -40,6 +26,4 @@ def signup():
                 flash('Password too short')
             else:
                 flash('Passwords do not match')
-
->>>>>>> 052341e8a75119f7129260ff60e92397019ab30b
     return render_template('signup.html', form = form)
