34432c9b1930b8d4ebdccee8376c8d7c660a794b
Michael <mhardy17@bayschoolsf.org>
2015-04-24 09:43:59 -0700

    fixed create post downgraded pymongo

--- tools/createPost.py
+++ tools/createPost.py
@@ -1,27 +1,27 @@
 #! tools-env/bin/python
 
-from mongoengine import connect, Document, EmailField, StringField, BooleanField, DateTimeField
+from mongoengine import connect, Document, EmailField, StringField, BooleanField, DateTimeField, ReferenceField, IntField
 
 connect(db='project',
     host = 'localhost',
     port = 27017)
 
-class User(db.Document):
-    email = db.EmailField(required = True, unique = True)
-    alias = db.StringField(required = True, unique = True)
-    password = db.StringField(min_length = 8, required = True)
-    emailVerified = db.BooleanField(default = False)
-    lastLogin = db.DateTimeField()
-    allowTracking = db.BooleanField(default = False)
-    isMod = db.BooleanField(default = False)
+class User(Document):
+    email = EmailField(required = True, unique = True)
+    alias = StringField(required = True, unique = True)
+    password = StringField(min_length = 8, required = True)
+    emailVerified = BooleanField(default = False)
+    lastLogin = DateTimeField()
+    allowTracking = BooleanField(default = False)
+    isMod = BooleanField(default = False)
 
-class Posts(db.Document):
+class Posts(Document):
     #max length of title is 140 characters
-    title = db.StringField(required = True, max_length = 140)
-    author = db.ReferenceField(User)
+    title = StringField(required = True, max_length = 140)
+    author = ReferenceField(User)
     #max length of content is 10000 characters
-    content = db.StringField(max_length = 10000)
-    score = db.IntField(default = 0)
+    content = StringField(max_length = 10000)
+    score = IntField(default = 0)
 
 alias = raw_input('alias: ')
 
@@ -30,8 +30,8 @@ title = raw_input('title: ')
 content = raw_input('post (must be under 1000 characters): ')
 
 try:
-    user = User.object(alias=alias)[0]  # returns user object by alias
-    post = Posts(user=user, title=title, content=content)
+    user = User.objects(alias=alias)[0]  # returns user object by alias
+    post = Posts(author=user, title=title, content=content)
 
     post.save()
     
