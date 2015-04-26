b7cca7bc6cfb49b41e3842c9f0b84715871711f8
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-25 19:55:19 -0700

    added more stuff for the api

--- application/api.py
+++ application/api.py
@@ -2,10 +2,17 @@
     This is going to confuse anyone who has ever used a restful interface before
     If you do not know what a restful interface is then you should not go any
     furthur.
+
+    As a furthur explanation this is a pitaful attempt to make a way for a computer
+    to easily interface with the website. The beauty of it is that there is no
+    ui needed, instead requests are responded to with json and http errors. If
+    any of that made sense to you then take a look.
 """
 
-from application import app
+from application import app, db
+from collections import User
 from flask.ext.restful import Api, Resource
+from flask import abort
 
 api = Api(app)
 
@@ -15,4 +22,14 @@ def apiUrlWrap(url):
 
 class ViewPosts(Resource):
     def get(self, user):
-        return {"hello":user}
+        try:
+            user = User.objects(alias = user)[0]
+        except IndexError:
+            abort(404)
+
+        try:
+            posts = collections.Posts.objects(author = User.objects(alias=user)[0])  # returns a list of posts by alias
+        except IndexError:
+            posts = None
+
+        return {"username":user.alias, "posts":posts}
