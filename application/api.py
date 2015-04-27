e45ac2998a2a3d19252b7cc203e968519d1be69b
Michael <mhardy17@bayschoolsf.org>
2015-04-27 11:19:59 -0700

    attempted to fix collisions

--- application/api.py
+++ application/api.py
@@ -1,9 +1,9 @@
 """
     This is going to confuse anyone who has ever used a restful interface before
     If you do not know what a restful interface is then you should not go any
-    further.
+    furthur.
 
-    As a further explanation this is a pitiful attempt to make a way for a computer
+    As a furthur explanation this is a pitaful attempt to make a way for a computer
     to easily interface with the website. The beauty of it is that there is no
     ui needed, instead requests are responded to with json and http errors. If
     any of that made sense to you then take a look.
@@ -26,14 +26,13 @@ class ViewPosts(Resource):
             user = User.objects(alias = user)[0]
         except IndexError:
             abort(404)
-
         try:
-            posts = Posts.objects(author = user)  # returns a list of posts by alias
-            titles = ''
+            posts = Posts.objects(author = user)
+            p = []
             for post in posts:
-                titles += post.title + ', '
-            titles = titles[0:len(titles)-2]
+                p.append(post)
         except IndexError:
-            titles = None
+            posts = None
 
-        return {"username":user.alias, "posts":titles}
+
+        return {"username":user.alias, "posts":p}
