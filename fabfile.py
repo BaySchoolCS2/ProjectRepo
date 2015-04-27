d1473b71992153b4eea8e8cf833d31dc703e8da6
Michael <mhardy17@bayschoolsf.org>
2015-04-27 12:17:36 -0700

    merge

--- fabfile.py
+++ fabfile.py
@@ -14,13 +14,5 @@ def setup_env():
 def pull():
     '''pull from remote and then try to install all requirements if they have
     changed
-<<<<<<< HEAD
     '''
-=======
-<<<<<<< HEAD
-    '''
-=======
-        '''
->>>>>>> e45ac2998a2a3d19252b7cc203e968519d1be69b
->>>>>>> 54def194b8d3f3f4c3711b075658bb96b625ba50
     local('./utils/pull.sh')
