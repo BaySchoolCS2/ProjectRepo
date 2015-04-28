e5f22772fcc4ffb4da31e717e01171202254f434
Helena <hbelhumeur16@bayschoolsf.org>
2015-04-28 12:53:26 -0700

    settings page!!! :)

--- application/settings.py
+++ application/settings.py
@@ -1,6 +1,11 @@
 from application import app, collections
-from flask import render_template
+from flask import render_template, session, redirect, url_for
+
+
 
 @app.route('/settings')
+
 def settings():
+    if not session.get('logged_in'):
+        return redirect (url_for('index'))
     return render_template('settings.html')
