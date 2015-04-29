f3d7f749781cf19f877465c315d43ecd92c4f6f5
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-28 19:10:00 -0700

    added api key to settings

--- application/settings.py
+++ application/settings.py
@@ -1,11 +1,23 @@
 from application import app, collections
 from flask import render_template, session, redirect, url_for
-
+from collections import User
+from os import urandom
 
 
 @app.route('/settings')
-
 def settings():
     if not session.get('logged_in'):
-        return redirect (url_for('index'))
-    return render_template('settings.html')
+        return redirect(url_for('index'))
+    user = User.objects(alias = session.get("alias")).get()
+
+    return render_template('settings.html', apikey = user.apiKey)
+
+@app.route('/generate_api_key')
+def generateapikey():
+    if not session.get("logged_in"):
+        return redirect(url_for('index'))
+    user = User.objects(alias = session.get("alias")).get()
+    api_key = urandom(16).encode('base64')
+    user.apiKey = api_key
+    user.save()
+    return redirect(url_for('settings'))
