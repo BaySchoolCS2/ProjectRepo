5e24f372b9ad4f5856e33127990df9a44cea5c10
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-05-03 03:34:56 -0700

    more profiling stuff

--- profiler.py
+++ profiler.py
@@ -1,8 +1,10 @@
-#! /venv/bin/python
+#! venv/bin/python
 
 from werkzeug.contrib.profiler import ProfilerMiddleware
 from application import app
 
+
+app.secret_key = "ProfilingKey"
 app.config['PROFILE'] = True
 app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
 app.run(debug = True)
