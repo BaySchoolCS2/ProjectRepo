99003a8deb6022d4f6dfaa79237f13f71e3e0f0a
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-05-07 10:31:56 -0700

    updated signup to make it less derpy

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired, Required
 
 class LoginForm(Form):
     '''
