030e0253e9fec4ee48f6cd4aa7944aa41f73fe70
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-05-07 18:38:52 -0700

    added csrf protection

--- application/__init__.py
+++ application/__init__.py
@@ -2,12 +2,13 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.moment import Moment
-
+from flask-wtf import CsrfProtect
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 db = MongoEngine(app)
 moment = Moment(app)
+CsrfProtect(app)
 
 
 app.jinja_env.globals["len"] = len # allow len to be used in templates
