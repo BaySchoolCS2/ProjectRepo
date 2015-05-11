06a346473a533d311f90d5a13056e2c259a99d2b
Matthew Penny <mpenny16@bayschoolsf.org>
2015-05-11 11:17:40 -0700

    added create to subscription object

--- application/dashboard.py
+++ application/dashboard.py
@@ -6,7 +6,7 @@ from flask import render_template, session, redirect, url_for
 def dashboard():
     if session.get("logged_in"):
         user = User.objects.get(alias = session.get("alias"))
-        Subscriptions.objects.get(user=user)
+        Subscriptions.objects.get_or_create(user=user)
         return 'derp'
         #posts = Posts.objects(author__in=subscriptions).order_by('-sticky', '-score', '-created_at')
         #return str(subscriptions)
