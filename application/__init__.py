5351586d56d84efd8266c77ca96183c872d8dbaf
Helena <hbelhumeur16@bayschoolsf.org>
2015-05-15 09:47:25 -0700

    started working on getting new password

--- application/__init__.py
+++ application/__init__.py
@@ -3,12 +3,15 @@ from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.moment import Moment, _moment
 from flask_wtf import CsrfProtect
+from flask.ext.mail import Mail, Message
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 db = MongoEngine(app)
 moment = Moment(app)
 CsrfProtect(app)
+mail=Mail(app)
+
 
 app.jinja_env.globals["len"] = len # allow len to be used in templates
 app.jinja_env.globals["now"] = datetime.utcnow()
