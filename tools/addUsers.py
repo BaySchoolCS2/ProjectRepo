5ea9d723ec808431c732581a48feeabd084b75cd
Michael <mhardy17@bayschoolsf.org>
2015-05-19 19:59:30 -0700

    ui changes

--- tools/addUsers.py
+++ tools/addUsers.py
@@ -30,4 +30,4 @@ for line in usernames_file:
     for line in usernames_file:
         print line
         pw_hash = generate_password_hash(line.split(":")[2])
-        User(email=line.split(":")[1], alias=line.split(":")[0], password=pw_hash, isMod=line.split(":")[3], isJudge=line.split(":")[4]).save()
+        User(email=line.split(":")[1], alias=line.split(":")[0], password=pw_hash, isMod=line.split(":")[3], isJudge=line.split(":")[4], emailVerified=line.split(":")[5]).save()
