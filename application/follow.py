22983e191edd5e7c92b77363f8db83fd9cb7ff74
Michael <mhardy17@bayschoolsf.org>
2015-05-20 14:03:51 -0700

    fixed following

--- application/follow.py
+++ application/follow.py
@@ -10,10 +10,25 @@ def follow(fuser=None):
         user = User.objects(alias = session.get("alias")).get()
         fuser = User.objects(alias = fuser).get()
     except IndexError:
+        abort(404) #If there is an error, return a 404 code
+    sub = Subscriptions.objects.get_or_create(user=user)[0]
+    if fuser not in sub.subscriptions:
+        sub.subscriptions.append(fuser)
+        sub.save()
+    return redirect(url_for('profile', name = fuser.alias))
+
+@app.route('/uFollow/<fuser>') #Defines URL for following users
+def uFollow(fuser=None):
+    if not session.get('logged_in'):
+        return redirect(url_for('login')) #If the user is not logged in, redirect them to the login page
+    try:
+        user = User.objects(alias = session.get("alias")).get()
+        fuser = User.objects(alias = fuser).get()
+    except IndexError:
         abort(404) #If there is an error, return a 404 code s
-    return Subscriptions.objects.get_or_create(user=user)
-    # if fuser not in following.subscriptions:
-    #     following.subscriptions.append(fuser)
-    #     following.save()
+    sub = Subscriptions.objects.get_or_create(user=user)[0]
+    if fuser not in sub.subscriptions:
+        sub.subscriptions.remove(fuser)
+        sub.save()
 
     return redirect(url_for('profile', name = fuser.alias))
