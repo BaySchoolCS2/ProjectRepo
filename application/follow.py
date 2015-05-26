d3a6745359260128f5b68eb73271d719e1a1b90c
Helena <hbelhumeur16@bayschoolsf.org>
2015-05-26 12:19:22 -0700

    okMerge branch 'master' of https://github.com/BaySchoolCS2/ProjectRepo

--- application/follow.py
+++ application/follow.py
@@ -7,18 +7,14 @@ def follow(fuser=None):
     if not session.get('logged_in'):
         return redirect(url_for('login')) #If the user is not logged in, redirect them to the login page
     try:
-        user = User.objects(alias = session.get("alias")).get()
-        fuser = User.objects(alias = fuser).get()
+        user = User.objects(alias = session.get("alias"))[0]
+        fuser = User.objects(alias = fuser)[0]
     except IndexError:
         abort(404) #If there is an error, return a 404 code
     sub = Subscriptions.objects.get_or_create(user=user)[0]
-    print(sub.subscriptions)
     if user not in sub.subscriptions:
-        sub.subscriptions.append(user)
-        print(sub.subscriptions[0].alias)
+        sub.subscriptions.append(fuser)
         sub.save()
-    else:
-        print('FAIL!')
     return redirect(url_for('profile', name = fuser.alias))
 
 @app.route('/uFollow/<fuser>') #Defines URL for following users
@@ -29,11 +25,9 @@ def uFollow(fuser=None):
         user = User.objects(alias = session.get("alias")).get()
         fuser = User.objects(alias = fuser).get()
     except IndexError:
-        abort(404) #If there is an error, return a 404 code s
+        abort(404) #If there is an error, return a 404 code
     sub = Subscriptions.objects.get_or_create(user=user)[0]
     if user in sub.subscriptions:
-        sub.subscriptions.remove(user)
-        print(sub.subscriptions)
+        sub.subscriptions.remove(fuser)
         sub.save()
-
     return redirect(url_for('profile', name = fuser.alias))
