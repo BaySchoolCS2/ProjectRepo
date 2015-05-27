b8406f194b593b967998bf83f5e1e7cd5256bb70
Edward Burke <ed@burkessf.com>
2015-05-27 13:51:42 -0700

    changes to dashboard?

--- application/dashboard.py
+++ application/dashboard.py
@@ -2,7 +2,7 @@ from application import app
 from collections import Subscriptions, User, Posts
 from flask import render_template, session, redirect, url_for
 
-@app.route('/dashboard')
+@app.route('/dashboard') #Defines URL for the dashboard
 def dashboard():
     if session.get("logged_in"):
         user = User.objects.get(alias = session.get("alias"))
