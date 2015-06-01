4f752008fff58dc93d9b4bc669d68f0a68788f3c
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-06-01 10:21:33 -0700

    moving stuff around

--- application/flag.py
+++ application/flag.py
@@ -1,4 +1,4 @@
-from application import app
+from application import app, mail
 from collections import Subscriptions, User, Posts
 from flask import render_template, session, redirect, url_for, abort, flash
 
@@ -8,11 +8,20 @@ def flag(type_=None, postid=None):
         abort(401)
     if type_  not in ["1","2","3","4"] or postid == None:
         abort(404)
-
     post = Posts.objects.get(postid=postid)
     post.flags += 1
     post.flagTypes.append(int(type_))
     post.save()
+
+    mods = User.objects.get(isMod = True)
+    mod_emails = []
+    for mod in mods:
+        mod_email.append(mod.email)
+
+    msg = Message("Post has been flagged",
+        sender="whrlplcs@gmail.com",
+        recipients=mod_email)
+
     flash("You have flagged {0} by {1}".format(post.title, post.author.alias))
     return redirect(url_for("index"))
 
